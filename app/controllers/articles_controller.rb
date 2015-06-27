c068ddf8e30b84c8409c2a9b1b48c58870a076b9
Amit Kumar <amit.kumar4436@gmail.com>
2015-06-28 02:09:30 +0530

    finishing up styles, new look

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,21 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
-	def destroy
-		@article = Article.find(params[:id])
-		@article.destroy
-		redirect_to articles_path
+	def new
+		@article =Article.new
 	end
+
+	def create
+		@article=Article.new
+		@article.title=params[:article][:title]
+		@article.save
+		redirect_to article_path(@article)
+
+	end
+
+	#def destroy
+		#@article = Article.find(params[:id])
+		#@article.destroy
+		#redirect_to articles_path
+	#end
 end
